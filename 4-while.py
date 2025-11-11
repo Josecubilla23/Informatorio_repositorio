c09aed8295291a02d7f88ee3250320b8d9062281
Josecubilla23 <106341894+Josecubilla23@users.noreply.github.com>
2025-11-11 09:58:00 -0300

    Delete 4-while.py

--- 4-while.py
+++
@@ -1,8 +0,0 @@
-contador = 0
-lim_max = 3
-
-while (contador != lim_max + 1):
-    print('El contador va por la iteracion', contador)
-    contador += 1 # contador = contador + 1
-
-print('Salimos de la iteracion')
